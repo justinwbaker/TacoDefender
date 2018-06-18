c7c2631e0c8f5b8ec0acd8e5f45b18befe282893
justin <justin.baker@easternstudent.ca>
2018-06-18 19:04:23 -0300

    TacoTruck Class start

---
+++ TacoDefender/Characters/TacoTruck.swift
@@ -0,0 +1,26 @@
+//
+//  TacoTruck.swift
+//  TacoDefender
+//
+//  Created by justin baker on 2018-06-18.
+//  Copyright © 2018 justin baker. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class TacoTruck: SKSpriteNode {
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Use init()")
+    }
+
+    init() {
+        let texture = SKTexture(imageNamed: "TacoTruck")
+        super.init(texture: texture, color: .white, size: texture.size())
+
+        name = "TacoTruck"
+        zPosition = 50
+    }
+
+}
