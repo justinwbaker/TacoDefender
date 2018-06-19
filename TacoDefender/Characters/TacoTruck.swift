e1312229957be647aa64747ae26f9514c40f964b
justin <justin.baker@easternstudent.ca>
2018-06-19 19:29:04 -0300

    Taco smaller in scene

--- TacoDefender/Characters/TacoTruck.swift
+++ TacoDefender/Characters/TacoTruck.swift
@@ -11,16 +11,21 @@ import SpriteKit
 
 class TacoTruck: SKSpriteNode {
 
+    var tacos = 5
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("Use init()")
     }
 
     init() {
         let texture = SKTexture(imageNamed: "TacoTruck")
-        super.init(texture: texture, color: .white, size: texture.size())
+        let size = CGSize(width: texture.size().width/5, height: texture.size().height/5)
+
+        super.init(texture: texture, color: .white, size: size)
 
         name = "TacoTruck"
-        zPosition = 50
+        zPosition = 3
+
     }
 
 }
