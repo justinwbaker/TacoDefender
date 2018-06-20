7fe502b601fc6d1730541e237d7f285d87486ad8
Cody Cormier <codyjcormier@codys-mac-n-cheese.local>
2018-06-20 19:20:51 -0300

    tower mostly done

--- TacoDefender/Towers/Tower.swift
+++ TacoDefender/Towers/Tower.swift
@@ -10,13 +10,81 @@ import Foundation
 import SpriteKit
 
 class Tower: SKSpriteNode {
+    
+    enum towerType {
+        case ketchup
+        case mustard
+        case guacamole
+        case limeJuice
+        case sourCream
+        case sriracha
+        case chili
+        case tartarSauce
+    }
+
+    
+    
+    
+    
+    var health: CInt
+    var maxHealth: CInt
+    var range: CInt
+    var firerate: CInt
+    var type: towerType
+    var target: CGPoint
+    
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("use init()")
     }
 
-    init() {
+    init(towerType tower: towerType, CGPoint position: CGPoint) {
         let texture = SKTexture(imageNamed: "TowerBase")
+        type = tower
+        switch type{
+            case.ketchup:
+                maxHealth = 5
+                range = 3
+                firerate = 2
+            
+            case.mustard:
+                maxHealth = 7
+                range = 3
+                firerate = 2
+            
+            case.limeJuice:
+                maxHealth = 5
+                range = 3
+                firerate = 4
+            
+            case.sourCream:
+                maxHealth = 5
+                range = 4
+                firerate = 5
+            
+            case.sriracha:
+                maxHealth = 10
+                range = 2
+                firerate = 8
+            
+            case.chili:
+                maxHealth = 0
+                range = 0
+                firerate = 0 // TODO
+            
+            case.guacamole:
+                maxHealth = 2
+                range = 10
+                firerate = 7
+            
+            case.tartarSauce:
+                maxHealth = 0
+                range = 0
+                firerate = 0 // TODO
+        }
+        health = maxHealth
+        target = CGPoint(x: 0, y: 0)
+        
         super.init(texture: texture, color: .white, size: texture.size())
     }
 
