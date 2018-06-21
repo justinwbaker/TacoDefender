1d8618d0c493639e401b2194d364b18a8cc93db2
Cody Cormier <codyjcormier@codys-mac-n-cheese.local>
2018-06-21 19:35:40 -0300

    PI

--- TacoDefender/GameScene.swift
+++ TacoDefender/GameScene.swift
@@ -19,7 +19,7 @@ extension CGPoint {
     func getAngle(CGPoint point: CGPoint) -> CGFloat {
         let dx = point.x - self.x
         let dy = point.y - self.y
-        return atan(dx/dy)
+        return atan(dx/dy) + CGFloat.pi
     }
 }
 
