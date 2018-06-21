faf20f756f220d30ae986b039aef9db64495e4f8
Cody Cormier <codyjcormier@codys-mac-n-cheese.local>
2018-06-21 18:59:34 -0300

    tower size and color based on tower type

--- TacoDefender/Towers/Tower.swift
+++ TacoDefender/Towers/Tower.swift
@@ -9,6 +9,11 @@
 import Foundation
 import SpriteKit
 
+func / (left: CGSize, right: CGFloat) -> CGSize{
+    
+    return CGSize(width: left.width / right, height: left.height / right)
+}
+
 class Tower: SKSpriteNode {
     
     enum towerType {
@@ -28,6 +33,8 @@ class Tower: SKSpriteNode {
     var firerate: CInt
     var type: towerType
     var target: CGPoint
+    var towerColor: SKColor
+    var colorBlend: CGFloat
     
 
     required init?(coder aDecoder: NSCoder) {
@@ -42,46 +49,76 @@ class Tower: SKSpriteNode {
                 maxHealth = 5
                 range = 3
                 firerate = 2
+                towerColor = .red
+                colorBlend = 1
+
             
             case.mustard:
                 maxHealth = 7
                 range = 3
                 firerate = 2
+                towerColor = .yellow
+                colorBlend = 1
+
             
             case.limeJuice:
                 maxHealth = 5
                 range = 3
                 firerate = 4
+                towerColor = .green
+                colorBlend = 0.25
+
+
             
             case.sourCream:
                 maxHealth = 5
                 range = 4
                 firerate = 5
+                towerColor = .white
+                colorBlend = 0.9
+
+
             
             case.sriracha:
                 maxHealth = 10
                 range = 2
                 firerate = 8
+                towerColor = .red
+                colorBlend = 0.5
+
+
             
             case.chili:
                 maxHealth = 0
                 range = 0
                 firerate = 0 // TODO
+                towerColor = .brown
+                colorBlend = 0.5
+
+
             
             case.guacamole:
                 maxHealth = 2
                 range = 10
                 firerate = 7
+                towerColor = .green
+                colorBlend = 0.8
+
+
             
             case.tartarSauce:
                 maxHealth = 0
                 range = 0
                 firerate = 0 // TODO
+                towerColor = .yellow
+                colorBlend = 0.25
+
+
         }
         health = maxHealth
         target = CGPoint(x: 0, y: 0)
-        super.init(texture: texture, color: .red, size: texture.size())
-        self.colorBlendFactor = 1
+        super.init(texture: texture, color: .white, size: (texture.size()/6))
+        self.colorBlendFactor = colorBlend
 
         self.position = position
         self.zRotation = self.position.getAngle(CGPoint: target)
